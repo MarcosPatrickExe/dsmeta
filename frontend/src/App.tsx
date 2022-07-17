407838e4e06c06f0be7d09643754226ba04646cd
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-07-17 16:01:20 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 //import react from 'react'
 import './App.css'
+import Notificacao from './components/NotificationButton/index';
+
 
 function App() {
  
-    return <h1>Olá mundõõõõõ!!!!! </h1>
+    return (
+        <>
+            <h1>Olá mundõõõõõ!!!!! </h1>
+            <Notificacao />
+            <Notificacao />
+        </>
+    )
 }
 
 export default App
