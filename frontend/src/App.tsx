27e98a5dd2c1a0dc7e710a5603b38215f3d0e2a3
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-07-18 17:53:53 -0300

    Datepicker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 //import react from 'react'
 import './App.css'
-import Notificacao from './components/NotificationButton/index';
 import Header from './components/Header/index';
 import SalesCard from './components/SalesCard/index';
 
@@ -21,3 +20,13 @@ function App() {
 }
 
 export default App;;
+
+// pagia semana spring react: 
+// https://devsuperior.com.br/evento-sds?episodio=1
+
+// link do repositorio:
+// https://github.com/devsuperior/sds-dsmeta
+
+// dsmeta html + css:
+// https://github.com/acenelio/dsmeta-css
+
